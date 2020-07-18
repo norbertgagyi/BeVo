fd64af44b057b4e934329aa04a019ade984f54de
Norbi G <norbert.gagyi@gmail.com>
2020-07-18 16:40:16 +0300

    Made switching screens functionality

--- BeVo game/Assets/Code/GameLogic.cs
+++ BeVo game/Assets/Code/GameLogic.cs
@@ -11,26 +11,66 @@ public class GameLogic : MonoBehaviour
     }
 
     [SerializeField]
-    List<string> players = new List<string>();
+    private List<string> players = new List<string>();
 
-    ChallengeGenerator challengeGenerator;
+    private ChallengeGenerator challengeGenerator;
+
+    private Transform screensParent;
+
+    [SerializeField]
+    private List<GameObject> screens = new List<GameObject>();
+    private GameObject currentScreen;
+
+    private GameObject previousScreen = null;
 
     private void Start()
     {
         challengeGenerator = transform.GetComponent<ChallengeGenerator>();
+
+        GetScreens();
+
+        if(screens.Count != 0)
+        {
+            currentScreen = screens[0];
+        }
+    }
+
+    private void GetScreens()
+    {
+        screensParent = GameObject.Find("Canvas").transform;
+
+        if(screensParent != null)
+        {
+            foreach(Transform screen in screensParent)
+            {
+                if(screen != null)
+                {
+                    screens.Add(screen.gameObject);
+                }
+            }
+        }
+    }
+
+    public void SwitchScreen(int index)
+    {
+        if(screens.Count != 0)
+        {
+            if(screens[index] != null)
+            {
+                previousScreen = currentScreen; //set here before currentScreen is assigned to
+                previousScreen.SetActive(false);
+                
+                currentScreen = screens[index];
+                currentScreen.SetActive(true);
+            }
+        }
     }
     
-    public void GenerateChallenge(string gameMode)
+    private void GenerateChallenge(string gameMode)
     {
         Debug.Log("challenge accepted");
 
         challengeGenerator.GenerateChallenge(players[Random.Range(0,players.Count)], "5");
-    }
-
-    public string Dothething()
-    {
-        Debug.Log("pizzicatto");
-        return "pizzicatto";
     }
 
 }
